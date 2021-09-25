5af3d3fa8d239a8cedad2ec2663bb058a5a98c92
Berat Asrın CAFEROĞLU <beratasrin01@gmail.com>
2021-09-25 13:39:27 +0300

    contact sayfasına başlanıldı devam edılecek

---
+++ js/navbar_script.js
@@ -0,0 +1,9 @@
+let navbarElement = document.getElementById("navbar");
+navbarElement.innerHTML = `<a id="logo" class="nav-link" href="index.html"><b>LOGO</b></a>
+<div id="links-of-navbar">
+  <a class="nav-link" href="#">Gallery</a>
+  <a class="nav-link" href="#">Menu</a>
+  <a class="nav-link" href="#">Branches</a>
+  <a class="nav-link" href="#">Who Are We?</a>
+  <a class="nav-link" href="contact.html">Contact</a>
+</div>`;
